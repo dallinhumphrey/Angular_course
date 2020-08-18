f9761f70f2b9f6fee922f218f79ab72a17fa57f7
dallinhumphrey <dallinhumphrey@gmail.com>
2020-08-18 10:36:25 -0600

    bindings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
@@ -9,7 +9,7 @@ import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [AppComponent, ServerComponent, ServersComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule, BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
